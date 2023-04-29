25338d9ed4812483b92eebde56816a5eb4b63234
resumeainewss <131764199+resumeainewss@users.noreply.github.com>
2023-04-29 19:21:03 -0300

    Create status.js

---
+++ TDGMER/status.js
@@ -0,0 +1,4 @@
+let hunger = 100; // fome do jogador
+let sleepiness = 100; // sono do jogador
+let isWorking = false; // se o jogador está trabalhando
+let isAvailable = true; // se o jogador está disponível para atuar no mercado
